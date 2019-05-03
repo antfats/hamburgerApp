7f48885a34b863be55ad93b86b4fcbaed3abd7d7
bob <antfats@hotmail.com>
2019-05-03 10:32:54 -0700

    fixing orms

--- config/orm.js
+++ config/orm.js
@@ -2,29 +2,29 @@ var connection = require(server);
 
 
 //SQL get name of burgers to post on page
-module.exports = function () {
+var orm = {
     burgerNames = function () {
-        connection.connect(function (err) {
+        let queryString = "SELECT burgerName from freshBurger";
+        connection.query(queryString, function (err, res) {
             if (err) throw err;
-            connection.query("SELECT burgerName FROM freshBurger", function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            })
+            console.log(res);
         })
-    }
+    },
 
 
 
     addBurger = function (name, bool) {
-        conncetion.connect(function (err) {
-            if (err) throw err;
-            connection.query("INSERT INTO freshBurger VALUES (?,?)", function (err, res) {
+        let queryString = "INSERT INTO freshburger VALUE ?";
+            connection.query(queryString[burgerName], function (err, res) {
                 if (err) throw err;
             });
         });
-    }
+    },
 
     eatBurger = function () {
         $("#eatBtn").click()
     }
 }
+module.exports = orm;
+
+
